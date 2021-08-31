dd5476710f762d9a39baa026febacce07c1f8d47
avagadro <j2cry.avagadro@gmail.com>
2021-08-31 23:03:21 +0300

    structure reorganized: one method = one action
    Add_item feature allocated to a separate method.

--- service.py
+++ service.py
@@ -6,39 +6,48 @@ app = Flask(__name__)
 
 @app.route('/<collection>', methods=['POST', 'GET'])
 def debts_service(collection):
-    """
+    """ Show HTML interface
         :param collection: mongo database collection name """
-    if request.method == 'POST':        # add record to database
-        debts = Debts(collection=collection)
-        record = dict(request.form)
-        record['sharers'] = record.get('sharers').split()
-        debts.push(record)
-
     debts = Debts(collection=collection)
     return render_template('data.html', collection=collection, records=debts.get_all())
 
 
-@app.route('/<collection>/result', methods=['POST'])
-def debts_result(collection):
+@app.route('/<collection>/add', methods=['POST'])
+def debts_add_item(collection):
+    """ Add record to database and redirect to main interface
+        :param collection: mongo database collection name """
     debts = Debts(collection=collection)
-    # TODO: html template for result output
-    return str(debts.get_debts())
+    record = dict(request.form)
+    record['sharers'] = record.get('sharers').split()
+    debts.push(record)
+    return redirect(f'/{collection}')
 
 
 @app.route('/<collection>/remove')
 def debts_remove_item(collection):
+    """ Remove record from database and redirect to main interface
+        :param collection: mongo database collection name """
     debts = Debts(collection=collection)
     if _id := request.args.get('_id', None):
         debts.remove(_id)
     return redirect(f'/{collection}')
 
 
-# ------------- DEBUG ----------------
-@app.route('/<collection>/clear')
-def debts_clear(collection):
+@app.route('/<collection>/result', methods=['POST'])
+def debts_result(collection):
+    """ Show result
+        :param collection: mongo database collection name """
     debts = Debts(collection=collection)
-    debts.clear()
-    return 'DEBUG: Database cleared'
+    # TODO: html template for result output
+    return render_template('result.html', collection=collection, _debts=debts.get_debts())
+
+
+# ------------- DEBUG ----------------
+# @app.route('/<collection>/clear')
+# def debts_clear(collection):
+#     debts = Debts(collection=collection)
+#     debts.clear()
+#     return 'DEBUG: Database cleared'
 
 
 if __name__ == '__main__':
